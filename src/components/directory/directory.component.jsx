f1e7520cfe88ed53881103e4367ac871ef69948b
prem <gaddameedipremkumar@gmail.com>
2020-12-10 04:20:12 -0800

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
             sections: [{
                     title: 'hats',
                     imageUrl: 'https://images.pexels.com/photos/35185/hats-fedora-hat-manufacture-stack.jpg?auto=compress&cs=tinysrgb&h=750&w=1260',
-                    id: 1
+                    id: 1,
+                linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://images.pexels.com/photos/1200822/pexels-photo-1200822.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://images.pexels.com/photos/904117/pexels-photo-904117.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://images.pexels.com/photos/1964970/pexels-photo-1964970.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }]
             }
         }
@@ -45,11 +50,11 @@ render() {
     return (
         <div className='directory-menu'>
          {
-         this.state.sections.map(({title, imageUrl, id, size}) => (
-             <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+         this.state.sections.map(({id, ...otherSectionProps }) => (
+             <MenuItem key={id} {...otherSectionProps}/>
          ))}
         </div>
-    )
+    );
 }
 
 }
